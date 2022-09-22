8b0d8ca3f3f1ec03177e2ba54156ef2e85fcddb4
Mamadou 2727 <mamadouk393@gmail.com>
2022-09-22 20:48:12 +0200

    new

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
-import Link from "next/link";
 // Import react scroll
 import { Link as LinkScroll } from "react-scroll";
-import ButtonOutline from "../misc/ButtonOutline.";
 import LogoVPN from "../../public/assets/ritech_logo.svg";
 
 const Header = () => {
@@ -23,7 +21,9 @@ const Header = () => {
       >
         <nav className="max-w-screen-xl px-6 sm:px-8 lg:px-16 mx-auto grid grid-flow-col py-3 sm:py-4">
           <div className="col-start-1 col-end-2 flex items-center">
+            <a href="/">
             <LogoVPN className="h-20 w-auto" />
+            </a>
           </div>
           <ul className="hidden lg:flex col-start-4 col-end-8 text-black-500  items-center">
             <LinkScroll
