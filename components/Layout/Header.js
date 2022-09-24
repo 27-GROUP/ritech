76e6bd09514e3d1ed40df1327b6d3ece31fb7aa1
Mamadou 27 Authentic <62474348+Mamadou2727@users.noreply.github.com>
2022-09-24 13:57:51 +0200

    merging branches (#5)
    * Create team header (#1)
    
    * Social network removed
    
    * Test branch (#4)
    
    * Create team header
    
    * Social network removed
    
    * Team section
    
    * Mk image added
    
    * Sako image added
    
    * Created team part
    
    Co-authored-by: Mamadou 27 Authentic <62474348+Mamadou2727@users.noreply.github.com>
    
    Co-authored-by: Gjuuuy <101482272+Gjuuuy@users.noreply.github.com>
    Co-authored-by: Abdoulaye Sako <sakoa1212@gmail.com>

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 // Import react scroll
 import { Link as LinkScroll } from "react-scroll";
 import LogoVPN from "../../public/assets/ritech_logo.svg";
+import NextLink from "next/link"
+
 import NextLink from 'next/link';
 
 const Header = () => {
@@ -100,7 +102,7 @@ const Header = () => {
               Catalog
             </LinkScroll>
             <LinkScroll
-              activeClass="contact"
+              activeClass="active"
               to="contact"
               spy={true}
               smooth={true}
@@ -119,6 +121,29 @@ const Header = () => {
             </LinkScroll>
             <NextLink
               className="px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative"
+              href="/team"
+            >
+              Meet our team
+            </NextLink>
+            <LinkScroll
+              activeClass="team"
+              to="team"
+              spy={true}
+              smooth={true}
+              duration={1000}
+              onSetActive={() => {
+                setActiveLink("team");
+              }}
+              className={
+                "px-4 py-2 mx-2 cursor-pointer animation-hover inline-block relative" +
+                (activeLink === "team"
+                  ? " text-orange-500 animation-active "
+                  : " text-black-500 hover:text-orange-500 ")
+              }
+            >
+              Meet our team
+            </LinkScroll>
+
               href='/team'
             >
               Meet our team
@@ -226,7 +251,7 @@ const Header = () => {
                   d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                 />
               </svg>
-              Products/Services
+              What We Do
             </LinkScroll>
             <LinkScroll
               activeClass="active"
@@ -258,8 +283,45 @@ const Header = () => {
                   d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
                 />
               </svg>
-              Products
+              Catalog
             </LinkScroll>
+            <LinkScroll
+              activeClass="active"
+              to="catalog"
+              spy={true}
+              smooth={true}
+              duration={1000}
+              onSetActive={() => {
+                setActiveLink("catalog");
+              }}
+              className={
+                "mx-1 sm:mx-2 px-3 sm:px-4 py-2 flex flex-col items-center text-xs border-t-2 transition-all " +
+                (activeLink === "catalog"
+                  ? "  border-orange-500 text-orange-500"
+                  : " border-transparent ")
+              }
+            >
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                />
+              </svg>
+              Contact Us
+            </LinkScroll>
+            <NextLink
+              href="/team"
+              >
+              Meet our team
+            </NextLink>
           </ul>
         </div>
       </nav>
---
+++ components/team_list.js
@@ -0,0 +1,20 @@
+const team_list = [
+     {
+       name: "Mamadou K. KEITA",
+       image: "/assets/mk.jpeg",
+       experience: "Computer Science, Robotics & AI, Co-founder of Totally Design Corp",
+       role:
+         "Founder, Board Member",
+       website: "https://mamadouk.me",
+     },
+     {
+       name: "Abdoulaye SAKO",
+       image: "/assets/sako.jpeg",
+       experience: "Computer Science, Robotics & AI",
+       role:
+         "Mechanical engineer",
+       website: "/",
+     },
+   ];
+   
+   export default team_list;
